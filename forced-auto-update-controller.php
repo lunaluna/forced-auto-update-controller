7c2ae2d4a0f690f6adfff82c024e455abd6d5e0e
lunaluna <web@lunalunadesign.com>
2024-12-24 15:38:32 +0900

    プラグインのメタデータに GitHub のリンクを追加

--- forced-auto-update-controller.php
+++ forced-auto-update-controller.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Forced Auto Update Controller
  * Description: Git 管理下でも本番環境（指定ドメインパターンに合致）だけは自動更新をすべて有効化し、テスト環境・ローカル環境を無効化するプラグイン。他のテーマやプラグインが自動更新を無効化していても最終的に上書きします。
- * Version:     1.0.2
+ * Version:     1.0.3
  * Author:      Hiroki Saiki
  * Author URI:  https://profiles.wordpress.org/lunaluna_dev/
  * License:     GPLv2 or later
@@ -33,3 +33,33 @@ $fauc_auto_update_controller = new FAUC_Auto_update_Controller();
 
 // 3. アンインストールフックの設定 (プラグイン削除時に実行).
 register_uninstall_hook( __FILE__, 'fauc_forced_auto_update_controller_uninstall' );
+
+/**
+ * (4) プラグイン一覧のメタ情報欄に GitHub へのリンクを追加する関数
+ *
+ * - plugin_row_meta フィルタを使い、プラグインの行にカスタムリンクを追加
+ *
+ * @param string[] $links  既存のリンク (詳細, 設定など)
+ * @param string   $file   プラグインのベースファイル名
+ * @return string[]        $links に追加した結果を返す
+ */
+function fauc_set_plugin_meta( $links, $file ) {
+
+	// このプラグインのベースファイルパス(ディレクトリ/ファイル名).
+	static $this_plugin;
+	$this_plugin = plugin_basename( __FILE__ );
+
+	// プラグイン一覧で $file が実際にこのプラグインを指しているかどうかをチェック.
+	if ( $file === $this_plugin ) {
+		// リンクを追加 (例: GitHub リポジトリのURL).
+		$links[] = sprintf(
+			'<a href="%s" target="_blank" rel="noopener noreferrer">%s</a>',
+			esc_url( 'https://github.com/lunaluna/forced-auto-update-controller' ),
+			esc_html__( 'GitHub', 'forced-auto-update-controller' )
+		);
+	}
+
+	return $links;
+}
+// フィルターフックを登録 (優先度10, 引数2つ)
+add_filter( 'plugin_row_meta', 'fauc_set_plugin_meta', 10, 2 );
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Tags: update
 Requires at least: 5.5
 Tested up to: 6.7
-Stable tag: 1.0.1
+Stable tag: 1.0.3
 Requires PHP: 7.4
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
@@ -14,6 +14,9 @@ Forced Auto update Controller is a custom WordPress plugin that allows you to co
 2. Activate the plugin through the \'Plugins\' menu in WordPress.
 
 == Changelog ==
+
+= 1.0.3 =
+* Add a GitHub link to the plugin metadata
 
 = 1.0.2 =
 * Fix a fatal error
